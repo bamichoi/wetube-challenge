7eafa8b7dc455ebaed5e2a7e84df19406fe0300b
beomseok.choi <cbs412@naver.com>
2021-09-22 09:23:00 +0900

    pug conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
+const fakeUser = {
+    username : "bami",
+    loggedIn : true,
+};
+
+export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser });
 export const search = (req, res) => res.send("Search"); 
 export const watch = (req, res) => res.render("watch", {pageTitle : "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle : "Edit"}) // export 를 붙여주면 한 모듈이 동시에 여러개를 export를 할 수 있다.
