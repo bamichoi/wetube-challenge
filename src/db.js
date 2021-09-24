c9cea220a7c8f043a236d7df49b8b58f686c6c26
beomseok.choi <cbs412@naver.com>
2021-09-24 09:29:26 +0900

    Create video model with Mongo DB and Mongoose

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+
+const db = mongoose.connection;
+
+const handelOpen = () => console.log("Connected to DB ✅");
+
+db.on("error", (error) => console.log("DB error ❌", error));
+db.once("open", handelOpen);
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db"
+import "./models/Video";
 import express, { Router } from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
